ba8469246718b90a7e28032f97d730654102dcdb
mrprabalkumar144-cell <mr.prabal.kumar144@gmail.com>
2026-04-28 23:30:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
-CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
+CDB_NAME = environ.get("CDB_NAME", "")
 
 # Database Information
 DB_URI = environ.get("DB_URI", "")
